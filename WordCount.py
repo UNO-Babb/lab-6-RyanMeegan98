5cbe8aa38b1074dda3900cfc974477aad89154e5
RyanMeegan98 <ryan.meegan1171@gmail.com>
2025-03-02 22:36:36 +0000

    Completed

--- WordCount.py
+++ WordCount.py
@@ -1,13 +1,26 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name:Ryan Meegan
+#Date:Mar 2 2025
+#Assignment:Word COunt
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
+  textFile = open("fish.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  charCount = 0
+
   for line in textFile:
-    print(line)
+    lineCount = lineCount + 1
+    words = line.split() 
+    for w in words:
+      wordCount = wordCount + 1
+    charCount = charCount + len(line)
+    
+
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
+  print("Characters:", charCount)
+
   
 
 if __name__ == '__main__':
